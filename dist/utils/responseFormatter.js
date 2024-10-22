bec0f40c97be35c9c86415a2f8268494a9d33873
tushil@moon <tushil@moontechnolabs.com>
2024-10-22 17:44:36 +0530

    remove build

--- dist/utils/responseFormatter.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sendResponse = void 0;
-// Response utility function
-const sendResponse = (res, { status, message, data, error }) => {
-    res.status(status).json(Object.assign(Object.assign({ status,
-        message }, (data && { data })), (error && { error })));
-};
-exports.sendResponse = sendResponse;
