cd8df2bae039c73f3809ba99e8a60d0a7b092c86
tushil@moon <tushil@moontechnolabs.com>
2024-10-22 17:01:08 +0530

    add json

--- dist/config/firebase.js
+++ dist/config/firebase.js
@@ -6,10 +6,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.fireapp = void 0;
 const app_1 = require("firebase-admin/app");
 const firebase_admin_1 = __importDefault(require("firebase-admin"));
-const path_1 = __importDefault(require("path"));
-const serviceAccountPath = path_1.default.join(__dirname, "angularchatapp-8b6c5-firebase-adminsdk-1q4f5-adca81438f.json");
-console.log(serviceAccountPath);
+const config = require('./angularchatapp-8b6c5-firebase-adminsdk-1q4f5-adca81438f.json');
 exports.fireapp = (0, app_1.initializeApp)({
-    credential: firebase_admin_1.default.credential.cert(serviceAccountPath),
+    credential: firebase_admin_1.default.credential.cert(config),
     projectId: "angularchatapp-8b6c5", // <FIREBASE_PROJECT_ID>
 });
--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,15 +1,9 @@
 import { applicationDefault, initializeApp } from "firebase-admin/app";
 import admin from "firebase-admin";
-import path from "path";
-
-const serviceAccountPath = path.join(
-  __dirname,
-  "angularchatapp-8b6c5-firebase-adminsdk-1q4f5-adca81438f.json"
-);
-console.log(serviceAccountPath);
+const config = require('./angularchatapp-8b6c5-firebase-adminsdk-1q4f5-adca81438f.json');
 
 export const fireapp = initializeApp({
-  credential: admin.credential.cert(serviceAccountPath),
+  credential: admin.credential.cert(config),
   projectId: "angularchatapp-8b6c5", // <FIREBASE_PROJECT_ID>
 });
- 
+  
