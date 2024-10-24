ac3af4930002764ad08eab711947079d0b137be2
tushil@moon <tushil@moontechnolabs.com>
2024-10-24 11:09:16 +0530

    fix notification

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -3,7 +3,7 @@ import { ServiceAccount } from "firebase-admin";
 
 const config: ServiceAccount = {
   projectId: process.env.FIREBASE_PROJECT_ID,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  privateKey: process.env.FIREBASE_PRIVATE_KEY,
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
 };
 export const fireapp = initializeApp({
